33ac10e6c0c89389a6e167f1d68b6d49345788f4
Iskender Usenkanov <iusenkanov@gmail.com>
2025-04-08 01:53:38 -0700

    sentry

--- app/main.py
+++ app/main.py
@@ -1,14 +1,20 @@
 import os
+import sentry_sdk
+from fastapi import FastAPI
+
 dsn = os.getenv("SENTRY_DSN")
 sentry_sdk.init(
     dsn=dsn,
     traces_sample_rate=1.0
 )
 
-from fastapi import FastAPI
-
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello from Kubernetes on AWS!"}
+
+@app.get("/error")
+def generate_error():
+    division_by_zero = 1 / 0
+    return {"message": "This won't be returned"}
