550331cf16adf10f90e5340cec063ae83d852edc
Iskender Usenkanov <iusenkanov@gmail.com>
2025-04-09 10:47:56 -0700

    sentry release

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
 dsn = os.getenv("SENTRY_DSN")
 sentry_sdk.init(
     dsn=dsn,
-    release=os.getenv("SENTRY_RELEASE", "unknown"),
+    release=os.getenv("SENTRY_RELEASE")
     traces_sample_rate=1.0
 )
 
