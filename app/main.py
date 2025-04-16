ce364a83bafbc8e6789346e3b16784eb0ce38aca
Iskender Usenkanov <iusenkanov@gmail.com>
2025-04-15 18:55:42 -0700

    fix

--- app/main.py
+++ app/main.py
@@ -3,22 +3,10 @@ import sentry_sdk
 from fastapi import FastAPI
 from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
 
-#dsn = os.getenv("SENTRY_DSN")
+dsn = os.getenv("SENTRY_DSN")
 sentry_sdk.init(
-    dsn="https://501a7f30735cc02fcefd7878630bf1f6@o4509091708928000.ingest.us.sentry.io/4509116861972480",
-    max_breadcrumbs=50,
-    debug=True,
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for tracing.
-    traces_sample_rate=1.0,
-    # Add request headers and IP for users,
-    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
-    send_default_pii=True,
-    # By default the SDK will try to use the SENTRY_RELEASE
-    # environment variable, or infer a git commit
-    # SHA as release, however you may want to set
-    # something more human-readable.
-    release="myapp@1.0.5",
+    dsn=dsn,
+    traces_sample_rate=1.0
 )
 
 app = FastAPI()
